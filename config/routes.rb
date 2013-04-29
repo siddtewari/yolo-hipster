0572bbd940caffe63447c223f97a3958975cf84e
siddtewari <sidtewari@gmail.com>
2013-04-29 11:09:40 -0400

    constraint added, dashboard added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Shouter::Application.routes.draw do
+  # https://github.com/thoughtbot/clearance/tree/v1.0.0.rc7
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'dashboards#show'
+  end
+
+
   root to: "homes#show" 
+
+  resource :dashboards, only: [:show]
 end
